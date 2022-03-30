5c11dc3199e2efd090694aa874270de323947cd6
Sweekriti Satpathy <swsat@microsoft.com>
2022-03-30 14:07:50 -0700

    ios startup  update

--- ArtAuction.iOS/AppDelegate.cs
+++ ArtAuction.iOS/AppDelegate.cs
@@ -11,18 +11,8 @@ namespace ArtAuction.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : MauiUIApplicationDelegate<Startup>
+    public partial class AppDelegate : MauiUIApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            return base.FinishedLaunching(app, options);
-        }
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
